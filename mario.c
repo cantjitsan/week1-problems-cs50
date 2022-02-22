7cc70571ca2d384c944b2d6c7e08225406836f85
Nway Nway Wint Aung <75038105+cantjitsan@users.noreply.github.com>
2022-02-22 21:25:42 +0630

    Add files via upload

---
+++ mario.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int x,s,h,x1,h1,height;
+    do{
+        height = get_int("enter number between 1 and 8\n");
+    }
+    while(height<=0 || height>=8);
+
+
+    for (x=1; x<= height; x++) //(x) printing row-lines
+    {
+        //first pyramid
+        for (s=x; s<=(height-1); s++) //(s) printing spaces
+        {
+            printf(" ");
+        }
+        for (h=1; h<=x; h++) //(h) printing hashes
+        {
+            printf("#");
+        }
+
+        printf("  "); //gap between two pyramids
+
+        //second pyramid
+        for (x1=1; x1<=x; x1++) //(x1) printing row-lines
+        {
+            for (h1=x1; h1<=x1; h1++) //(h1) printing hashes
+            {
+                printf("#");
+            }
+        }
+
+        printf("\n"); //new lines
+    }
+    return 0;
+}
